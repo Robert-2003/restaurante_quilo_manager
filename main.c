9987f2c8fba197a888b6ef36bf10d469161414da
Robert <siqueirarobert2@gmail.com>
2025-04-02 11:03:59 -0300

    Iniciando
    Criando arquivo main.c e testando commits no GitHub

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <string.h>
+#include <dirent.h>
+#include <locale.h>
+
+int main(void) {
+	
+	printf("testando");
+	
+	return 0;
+}
